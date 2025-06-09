c0553e4ebdfa78bc2fd9524d930061181088babd
YutoGCN <61838168+YutoGCN@users.noreply.github.com>
2025-06-09 15:21:41 +0900

    update lecture13-2

--- 13_scientific/10_cavity_mpi.cpp
+++ 13_scientific/10_cavity_mpi.cpp
@@ -3,13 +3,14 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <mpi.h>
 
 using namespace std;
 typedef vector<vector<float>> matrix;
 
-int main() {
-  int nx = 41;
-  int ny = 41;
+int main(int argc, char** argv) {
+  int nx = 500;
+  int ny = 500;
   int nt = 500;
   int nit = 50;
   double dx = 2. / (nx - 1);
@@ -17,6 +18,13 @@ int main() {
   double dt = .01;
   double rho = 1.;
   double nu = .02;
+
+  int rank, size;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  int begin = rank * ny / size;
+  int end = (rank + 1) * ny / size;
 
   matrix u(ny,vector<float>(nx));
   matrix v(ny,vector<float>(nx));
@@ -37,15 +45,15 @@ int main() {
   ofstream vfile("v.dat");
   ofstream pfile("p.dat");
   for (int n=0; n<nt; n++) {
-    for (int j=1; j<ny-1; j++) {
-      for (int i=1; i<nx-1; i++) {
-        b[j][i] = rho * (1.0/dt * (
-                      (u[j][i+1] - u[j][i-1]) / (2*dx) +
-                      (v[j+1][i] - v[j-1][i]) / (2*dy)
+    for (int j = std::max(1, begin); j < std::min(ny - 1, end); j++) {
+      for (int i = 1; i < nx - 1; i++) {
+        b[j][i] = rho * (1.0 / dt * (
+                      (u[j][i + 1] - u[j][i - 1]) / (2 * dx) +
+                      (v[j + 1][i] - v[j - 1][i]) / (2 * dy)
                     )
-                    - pow((u[j][i+1] - u[j][i-1]) / (2*dx), 2)
-                    - 2.0 * ((u[j+1][i] - u[j-1][i]) / (2*dy)) * ((v[j][i+1] - v[j][i-1]) / (2*dx))
-                    - pow((v[j+1][i] - v[j-1][i]) / (2*dy), 2)
+                    - pow((u[j][i + 1] - u[j][i - 1]) / (2 * dx), 2)
+                    - 2.0 * ((u[j + 1][i] - u[j - 1][i]) / (2 * dy)) * ((v[j][i + 1] - v[j][i - 1]) / (2 * dx))
+                    - pow((v[j + 1][i] - v[j - 1][i]) / (2 * dy), 2)
                   );
       }
     }
@@ -53,12 +61,12 @@ int main() {
       for (int j=0; j<ny; j++)
         for (int i=0; i<nx; i++)
 	  pn[j][i] = p[j][i];
-      for (int j=1; j<ny-1; j++) {
-        for (int i=1; i<nx-1; i++) {
-          p[j][i] = (dy*dy * (pn[j][i+1] + pn[j][i-1]) +
-                     dx*dx * (pn[j+1][i] + pn[j-1][i]) -
-                     b[j][i] * dx*dx * dy*dy)
-                    / (2.0 * (dx*dx + dy*dy));
+      for (int j = std::max(1, begin); j < std::min(ny - 1, end); j++) {
+        for (int i = 1; i < nx - 1; i++) {
+          p[j][i] = (dy * dy * (pn[j][i + 1] + pn[j][i - 1]) +
+                     dx * dx * (pn[j + 1][i] + pn[j - 1][i]) -
+                     b[j][i] * dx * dx * dy * dy)
+                    / (2.0 * (dx * dx + dy * dy));
         }
       }
       // 境界条件
@@ -77,21 +85,57 @@ int main() {
 	vn[j][i] = v[j][i];
       }
     }
-    for (int j=1; j<ny-1; j++) {
-      for (int i=1; i<nx-1; i++) {
+    for (int j = std::max(1, begin); j < std::min(ny - 1, end); j++) {
+      for (int i = 1; i < nx - 1; i++) {
         u[j][i] = un[j][i]
-          - un[j][i] * dt / dx * (un[j][i] - un[j][i-1])
-          - vn[j][i] * dt / dy * (un[j][i] - un[j-1][i])
-          - dt / (2.0 * rho * dx) * (p[j][i+1] - p[j][i-1])
-          + nu * dt / (dx*dx) * (un[j][i+1] - 2.0 * un[j][i] + un[j][i-1])
-          + nu * dt / (dy*dy) * (un[j+1][i] - 2.0 * un[j][i] + un[j-1][i]);
+          - un[j][i] * dt / dx * (un[j][i] - un[j][i - 1])
+          - vn[j][i] * dt / dy * (un[j][i] - un[j - 1][i])
+          - dt / (2.0 * rho * dx) * (p[j][i + 1] - p[j][i - 1])
+          + nu * dt / (dx * dx) * (un[j][i + 1] - 2.0 * un[j][i] + un[j][i - 1])
+          + nu * dt / (dy * dy) * (un[j + 1][i] - 2.0 * un[j][i] + un[j - 1][i]);
 
         v[j][i] = vn[j][i]
-          - un[j][i] * dt / dx * (vn[j][i] - vn[j][i-1])
-          - vn[j][i] * dt / dy * (vn[j][i] - vn[j-1][i])
-          - dt / (2.0 * rho * dx) * (p[j+1][i] - p[j-1][i])
-          + nu * dt / (dx*dx) * (vn[j][i+1] - 2.0 * vn[j][i] + vn[j][i-1])
-          + nu * dt / (dy*dy) * (vn[j+1][i] - 2.0 * vn[j][i] + vn[j-1][i]);
+          - un[j][i] * dt / dx * (vn[j][i] - vn[j][i - 1])
+          - vn[j][i] * dt / dy * (vn[j][i] - vn[j - 1][i])
+          - dt / (2.0 * rho * dx) * (p[j + 1][i] - p[j - 1][i])
+          + nu * dt / (dx * dx) * (vn[j][i + 1] - 2.0 * vn[j][i] + vn[j][i - 1])
+          + nu * dt / (dy * dy) * (vn[j + 1][i] - 2.0 * vn[j][i] + vn[j - 1][i]);
+      }
+    }
+    if (size > 1) {
+      MPI_Status status;
+      // u
+      if (rank > 0) {
+        MPI_Sendrecv(&u[begin][0], nx, MPI_FLOAT, rank - 1, 0,
+                     &u[begin - 1][0], nx, MPI_FLOAT, rank - 1, 0,
+                     MPI_COMM_WORLD, &status);
+      }
+      if (rank < size - 1) {
+        MPI_Sendrecv(&u[end - 1][0], nx, MPI_FLOAT, rank + 1, 0,
+                     &u[end][0], nx, MPI_FLOAT, rank + 1, 0,
+                     MPI_COMM_WORLD, &status);
+      }
+      // v
+      if (rank > 0) {
+        MPI_Sendrecv(&v[begin][0], nx, MPI_FLOAT, rank - 1, 1,
+                     &v[begin - 1][0], nx, MPI_FLOAT, rank - 1, 1,
+                     MPI_COMM_WORLD, &status);
+      }
+      if (rank < size - 1) {
+        MPI_Sendrecv(&v[end - 1][0], nx, MPI_FLOAT, rank + 1, 1,
+                     &v[end][0], nx, MPI_FLOAT, rank + 1, 1,
+                     MPI_COMM_WORLD, &status);
+      }
+      // p
+      if (rank > 0) {
+        MPI_Sendrecv(&p[begin][0], nx, MPI_FLOAT, rank - 1, 2,
+                     &p[begin - 1][0], nx, MPI_FLOAT, rank - 1, 2,
+                     MPI_COMM_WORLD, &status);
+      }
+      if (rank < size - 1) {
+        MPI_Sendrecv(&p[end - 1][0], nx, MPI_FLOAT, rank + 1, 2,
+                     &p[end][0], nx, MPI_FLOAT, rank + 1, 2,
+                     MPI_COMM_WORLD, &status);
       }
     }
     for (int j=0; j<ny; j++) {
@@ -124,4 +168,5 @@ int main() {
   ufile.close();
   vfile.close();
   pfile.close();
+  MPI_Finalize();
 }
